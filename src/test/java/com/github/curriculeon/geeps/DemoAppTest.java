a4152a2bb50a18dc31edd82c8ab4e67ccfb9d3d0
leon hunter <xleonhunter@gmail.com>
2020-04-02 23:57:55 -0400

    repackaged

--- src/test/java/com/github/curriculeon/geeps/DemoAppTest.java
+++ src/test/java/com/github/curriculeon/geeps/DemoAppTest.java
@@ -1,6 +1,6 @@
-package com.github.git_leon.jetpack_survivor_maven;
+package com.github.curriculeon.geeps;
 
-import com.github.git_leon.greeps.Earth;
+import com.github.curriculeon.greeps.Earth;
 import com.github.git_leon.jfoot.system.JFootApplication;
 import greenfoot.Greenfoot;
 import greenfoot.core.WorldHandler;
